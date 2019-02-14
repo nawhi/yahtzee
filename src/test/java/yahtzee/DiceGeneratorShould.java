fb146902da0f11227024e6cd6f8d3b04e455ca7f
nawhi <25394965+nawhi@users.noreply.github.com>
2019-02-14 11:51:52 +0000

    Add another test to Dice generator

--- src/test/java/yahtzee/DiceGeneratorShould.java
+++ src/test/java/yahtzee/DiceGeneratorShould.java
@@ -1,5 +1,6 @@
 package yahtzee;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,14 +11,32 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class DiceGeneratorShould {
+
+    private Dice dice;
+    private DiceGenerator diceGenerator;
+
+    @BeforeEach
+    void setUp() {
+        dice = mock(Dice.class);
+        diceGenerator = new DiceGenerator(dice);
+    }
+
     @Test
-    void return_list_of_n_dice_rolls() {
+    void return_list_of_5_dice_rolls() {
         var expectedHand = asList(1,2,3,4,5);
-        var dice = mock(Dice.class);
         when(dice.roll()).thenReturn(1,2,3,4,5);
-        var diceGenerator = new DiceGenerator(dice);
 
         List<Integer> actualHand = diceGenerator.rollTimes(5);
+
+        assertEquals(expectedHand, actualHand);
+    }
+
+    @Test
+    void return_list_of_3_dice_rolls() {
+        var expectedHand = asList(4,6,2);
+        when(dice.roll()).thenReturn(4,6,2);
+
+        List<Integer> actualHand = diceGenerator.rollTimes(3);
 
         assertEquals(expectedHand, actualHand);
     }
